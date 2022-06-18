52292ff9c5324c9efc5d8944669f309c7da0701d
Utkarsh Gupta <utkarshgupta0408@gmail.com>
2022-06-18 15:19:16 +0530

    Test Summation

---
+++ src/main/java/Summation.java
@@ -0,0 +1,10 @@
+
+public class Summation {
+	
+	public int add(int n1,int n2)
+	{
+		return n1+n2;
+	}
+		
+
+}
---
+++ src/test/java/TestSummation.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestSummation {
+	
+	@Test
+	public void testAddition()
+	{
+		Summation sum = new Summation();
+		assertEquals(6,sum.add(2, 3));
+	
+	}
+
+}
